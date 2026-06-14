61eb378503d8380404f3cff1911346d95855fa78
Hardik Sigdel <hardisigdel361@gmail.com>
2026-06-15 01:14:57 +0545

    add user and video model

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from "express"
-import cors from "cors"
+import cors from "cors" 
 import cookieParser from "cookie-parser"
 
 const app = express()
@@ -11,8 +11,9 @@ app.use(cors({
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
+app.use(express.static)
 app.use(cookieParser())
 
 
-export {app}
+export {app} 
+
