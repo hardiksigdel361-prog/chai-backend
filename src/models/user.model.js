206ef606a0cf54268472c87c95724b67e53ab2dd
Hardik Sigdel <hardisigdel361@gmail.com>
2026-06-29 11:45:40 +0545

    Add models for comments, likes, playlists, and tweets

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken"
 import bcrypt from 'bcrypt'
 const userSchema = new Schema(
     {
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,5 +1,5 @@
 import mongoose, {Schema} from "mongoose";
-
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 import jwtoken from "jsonwebtoken"
 
 import bcrypt from "bcryptjs"
